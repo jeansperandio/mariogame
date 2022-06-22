322a42b55a390b75f6f036c24111fe5e26fefc8f
sperax <sperax@live.com>
2022-06-21 22:07:39 -0300

    jsoutros

--- js/scriptoutros.js
+++ js/scriptoutros.js
@@ -1,12 +1,17 @@
 const bala = document.querySelector(".bala-game");
 
 const loopGame = setInterval(() => { 
-  const balaPosition = b1.offsetLeft;
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+  mario.classList.remove("jump-mario");
+
+  setTimeout(() => {
+    mario.classList.remove(jump-mario");
+  },500);
+  //const balaPosition = bala.offsetLeft;
+  //const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
  
-  if (balaPosition <= 00 && balaPosition > 0 && marioPosition < 180) {
-    b1.style.animation = "none";
-    b1.style.left = `${b1Position}px`;
+  if (balaPosition <= 120 && balaPosition > 0 && marioPosition < 180) {
+    bala.style.animation = "none";
+    bala.style.left = `${balaPosition}px`;
  
     mario.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
